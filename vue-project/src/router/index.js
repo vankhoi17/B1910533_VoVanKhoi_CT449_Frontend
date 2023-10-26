9f68ab3c6e020f62dd712088d1adfbbc1a691e92
vovankhoi <khoib1910533@student.ctu.edu.vn>
2023-10-27 00:37:25 +0700

    Tao trang cap nhat lien he

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -6,6 +6,17 @@ path: "/",
 name: "contactbook",
 component: ContactBook,
 },
+{
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: true // Truyền các biến trong $route.params vào làm props
+    },
+{
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+    },
 ];
 const router = createRouter({
 history: createWebHistory(import.meta.env.BASE_URL),
--- vue-project/src/views/ContactBook.vue
+++ vue-project/src/views/ContactBook.vue
@@ -30,8 +30,7 @@
                     <i class="fas fa-address-card"></i>
                 </h4>
                 <ContactCard :contact="activeContact" />
-
-                <router-link
+<router-link
 :to="{
 name: 'contact.edit',
 params: { id: activeContact._id },
